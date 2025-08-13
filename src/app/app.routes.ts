bbb6ca4594d75758da8438b5f23be84787bb243b
Miguel Leandro <ledmiguel@gmail.com>
2025-08-13 17:16:56 -0500

    Actualizacion barra busqueda y banner

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { ProductoFormulario } from './pages/producto-formulario/producto-formula
 import { CategoriaFormulario } from './pages/categoria-formulario/categoria-formulario';
 import { Login } from './pages/login/login';
 import { Home } from './pages/home/home';
+import { Home2 } from './pages/home2/home2';
 import { About } from './pages/about/about';
 
 export const routes: Routes = [
     {path: '' , component: Home},
+    {path: 'inicio', component: Home2},
     {path: 'catalogo', component: Catalogo },    
     {path: 'nosotros', component: About },
     {path: 'agregar-producto', component: ProductoFormulario },
